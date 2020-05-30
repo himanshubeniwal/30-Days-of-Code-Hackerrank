a269bbb20c6e569cde888527e05057c0a77322fa
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2020-05-30 22:43:43 +0530

    Added Day 10 ✔

--- Day 10: Binary Number.py
+++ Day 10: Binary Number.py
@@ -6,16 +6,10 @@ import random
 import re
 import sys
 
-# Complete the factorial function below.
-def factorial(n):
-    if(n<1):
-        return 1
-    else:
-        return(n * factorial(n-1))
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
     n = int(input())
-    result = factorial(n)
-    fptr.write(str(result) + '\n')
-    fptr.close()
+    randomnmbr = bin(n).replace('0b', '0')
+    maxii = max(map(len, randomnmbr.split('0')))
+    print(maxii)
